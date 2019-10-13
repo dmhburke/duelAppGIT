acf7ab2dabc7d0fc147fa473ea0dbf28e138bb9b
Daniel Burke <dmhburke@Daniels-Air.nyc.rr.com>
2019-10-13 18:50:04 -0400

    Update admin test

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalog.models import PlayerModel
+
+# Define new admin class - PLAYER
+class PlayerModelAdmin(admin.ModelAdmin):
+     list_display = ('number', 'name', 'HC','total',)
+     ordering = ('-total', 'number',)
+
+# Register admin class
+admin.site.register(PlayerModel, PlayerModelAdmin)
