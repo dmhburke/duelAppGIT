d379b138d83b5957a1589928fc323d5b9b2e6ad7
Daniel Burke <dmhburke@Daniels-Air.nyc.rr.com>
2019-10-13 18:16:22 -0400

    Update no extension URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('',views.testpage, name='login')
-path('testpage/', views.testpage, name='testpage'),
+    path('', views.login, name='login'),
+    path('testpage/', views.testpage, name='testpage'),
 ]
